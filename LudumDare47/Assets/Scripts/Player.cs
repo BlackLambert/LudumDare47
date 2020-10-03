919d94ad3119fd0bb395b01865277c2ea1e76782
Sebastian Baier HomePC <sebastian.baier93@hotmail.de>
2020-10-03 15:58:11 +0200

    Adds player dash

---
+++ LudumDare47/Assets/Scripts/Player.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D _rigidbody;
+    public Rigidbody2D Rigidbody { get { return _rigidbody; } }
+
+}
--- LudumDare47/Assets/Scripts/PlayerInput.cs
+++ LudumDare47/Assets/Scripts/PlayerInput.cs
@@ -1,18 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+    private PointerInput _pointerInput;
+    [SerializeField]
+    private Player _player = null;
+    [SerializeField]
+    private float _dashForce = 2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _pointerInput = FindObjectOfType<PointerInput>();
+        if (_pointerInput == null)
+            throw new NullReferenceException("There is no pointer input object in the scene");
+        _pointerInput.OnClick += onClick;
+        _pointerInput.OnDragFinished += onDrag;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void onClick(ClickEventArgs args)
     {
-        
+        Debug.Log($"Splash!");
+    }
+
+    private void onDrag(DragEventArgs args)
+    {
+        _player.Rigidbody.AddForce(args.Direction.normalized * _dashForce);
     }
 }
