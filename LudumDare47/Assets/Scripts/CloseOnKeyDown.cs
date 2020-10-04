57ee7cd950f77f5ebacaa1972b1c7662c87e4515
Sebastian Baier HomePC <sebastian.baier93@hotmail.de>
2020-10-04 19:52:08 +0200

    Adds Game Cancle possibility.

---
+++ LudumDare47/Assets/Scripts/CloseOnKeyDown.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseOnKeyDown : MonoBehaviour
+{
+    [SerializeField]
+    private KeyCode _key = KeyCode.Escape;
+
+    protected virtual void Update()
+	{
+        if (Input.GetKeyDown(_key))
+            Application.Quit();
+	}
+}
