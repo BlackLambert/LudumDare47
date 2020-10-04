2e9da31bb5c2dd78999797ce5826217c9438edbc
Sebastian Baier HomePC <sebastian.baier93@hotmail.de>
2020-10-04 22:27:36 +0200

    Adds Larger Splashes. Adds Whale and WaterBall Objects

---
+++ LudumDare47/Assets/Scripts/Splash.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splash : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _front;
+    [SerializeField]
+    private Animator _back;
+    [SerializeField]
+    private string _triggerName = "Splash";
+    [SerializeField]
+    private GameObject _base;
+    [SerializeField]
+    private float _destroyAfterSeconds = 1f;
+
+    protected virtual void Start()
+	{
+        StartCoroutine(trigger());
+    }
+
+    private IEnumerator trigger()
+	{
+        _front.SetTrigger(_triggerName);
+        _back.SetTrigger(_triggerName);
+        yield return new WaitForSeconds(_destroyAfterSeconds);
+        Destroy(_base);
+    }
+}
