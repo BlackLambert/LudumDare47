19c311478b67f092af1a014c621df0b3e69ec854
Sebastian Baier HomePC <sebastian.baier93@hotmail.de>
2020-10-05 18:15:33 +0200

    Adds TMPro. Adds end screen.

---
+++ LudumDare47/Assets/Scripts/CloseGameButton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CloseGameButton : MonoBehaviour
+{
+    [SerializeField]
+    private Button _button = null;
+
+    protected virtual void Start()
+	{
+		_button.onClick.AddListener(close);
+
+    }
+
+	protected virtual void OnDestroy()
+	{
+		_button.onClick.RemoveListener(close);
+	}
+
+	private void close()
+	{
+		Application.Quit();
+	}
+}
---
+++ LudumDare47/Assets/Scripts/SetFloatOnBarrierProgess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetFloatOnBarrierProgess : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _animator = null;
+    [SerializeField]
+    private string _floatName = "FillState";
+
+    private BavariaOneLevel _level;
+
+	protected void Start()
+	{
+		_level = FindObjectOfType<BavariaOneLevel>();
+		if (_level == null)
+			throw new NullReferenceException("There has to be a BavariaOneLevel component in the scene!");
+		_level.OnBarrierProgressChanged += updateFloat;
+	}
+
+	protected virtual void OnDestroy()
+	{
+		_level.OnBarrierProgressChanged -= updateFloat;
+	}
+
+	private void updateFloat()
+	{
+		_animator.SetFloat(_floatName, _level.BarrierProgress);
+	}
+}
