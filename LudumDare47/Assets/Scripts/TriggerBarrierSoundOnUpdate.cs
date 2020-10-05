e00e9423cadb6e63f54a737c64983fa02727d99a
Franziska Schneider <franziska.schneider@me.com>
2020-10-05 22:28:07 +0200

    yodiyodiyodioa

---
+++ LudumDare47/Assets/Scripts/TriggerBarrierSoundOnUpdate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TriggerBarrierSoundOnUpdate : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource _triggerSound;
+
+    private BavariaOneLevel _level;
+
+    protected void Start()
+    {
+        _level = FindObjectOfType<BavariaOneLevel>();
+        if (_level == null)
+            throw new NullReferenceException("There has to be a BavariaOneLevel component in the scene!");
+        _level.OnBarrierProgressChanged += triggerSound;
+    }
+
+    protected virtual void OnDestroy()
+    {
+        _level.OnBarrierProgressChanged -= triggerSound;
+    }
+
+    private void triggerSound()
+    {
+        _triggerSound.Play();
+    }
+}
